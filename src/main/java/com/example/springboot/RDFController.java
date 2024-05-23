903df8a7ec448bb950924156228d91bb0c2c97ef
Zihan Chen <zihan1048@live.com>
2024-05-23 13:09:03 +0200

    feat: allow only POST request to the api

--- src/main/java/com/example/springboot/RDFController.java
+++ src/main/java/com/example/springboot/RDFController.java
@@ -2,10 +2,7 @@ package com.example.springboot;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -13,7 +10,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(methods = {RequestMethod.POST})
 public class RDFController {
     @PostMapping("/generateLinkedData")
     public ResponseEntity<?> generateLinkedData(@RequestParam("yamlFile") MultipartFile yamlFile,
